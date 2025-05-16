a399448d02b06f599660258c1f6195f26b7ae5b9
songyanannba <1499067030@qq.com>
2025-05-16 17:46:40 +0800

    动物派对

--- gateway/routers/slot_routers.go
+++ gateway/routers/slot_routers.go
@@ -13,6 +13,7 @@ var SlotRouter = slotRouter{}
 func (rm *slotRouter) SlotRouterInit() {
 	//登录
 	websocket.RegisterProto(int32(pbs.ProtocNum_LoginReq), websocket.Login)
+
 	//心跳
 	websocket.RegisterProto(int32(pbs.ProtocNum_HeartReq), websocket.Heartbeat)
 
