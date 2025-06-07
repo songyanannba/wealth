659560a33b162c4cf8b6aa43f95539a17a1057f3
songyanannba <1499067030@qq.com>
2025-06-07 14:35:55 +0800

    动物派对

--- gateway/README.md
+++ gateway/README.md
@@ -1,6 +1,7 @@
 # slot_game_server
 
 ##  项目编译
-
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o  gateway_linux
+    nohup ./gateway_linux  > /dev/null  2>&1 &
 
 
--- gateway/routers/routers.go
+++ gateway/routers/routers.go
@@ -7,11 +7,12 @@ import (
 
 // WebsocketInit Websocket 路由
 func WebsocketInit() {
+	//websocket.Register("login", websocket.LoginController)
+	//websocket.Register("heartbeat", websocket.HeartbeatController)
 
 	//proto 协议路由
 	websocket.RegisterProto(1, websocket.ProtoTestController)
 	websocket.RegisterProto(2, websocket.ProtoTest2Controller)
-	//websocket.Register("3", websocket.MTStatus)
 
 	SlotRouter.SlotRouterInit()
 
